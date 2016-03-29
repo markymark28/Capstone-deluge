d8c3956fa689e0f91b33a04c533627275dccea4f
Bayless <m160426@usna.edu>
2016-03-28 17:30:04 -0700

    disperse list with fixes

--- deluge/ui/gtkui/sendtorrentdialog.py
+++ deluge/ui/gtkui/sendtorrentdialog.py
@@ -25,7 +25,7 @@ from deluge.ui.gtkui.dialogs import ErrorDialog
 from deluge.ui.gtkui.path_chooser import PathChooser
 from deluge.ui.gtkui.torrentview_data_funcs import cell_data_size
 from deluge.ui.common import TorrentInfo
-
+import csv
 
 log = logging.getLogger(__name__)
 
@@ -122,27 +122,13 @@ class SendTorrentDialog:
         self.listview_torrents.set_model(self.torrent_liststore)
         self.listview_torrents.set_tooltip_column(2)
         
-        self.core_keys = [
-            "pre_allocate_storage",
-            "max_connections_per_torrent",
-            "max_upload_slots_per_torrent",
-            "max_upload_speed_per_torrent",
-            "max_download_speed_per_torrent",
-            "prioritize_first_last_pieces",
-            "sequential_download",
-            "add_paused",
-            "download_location",
-            "download_location_paths_list",
-            "move_completed",
-            "move_completed_path",
-            "move_completed_paths_list",
-        ]
+      
         
         #name, email
         
         #create liststore
         self.liststore = self.builder.get_object("liststore1")
-        
+        self.liststore.clear()
         #create treeview using liststore
         #self.treeview  = gtk.TreeView(self.liststore)
         self.treeview = self.builder.get_object("listview_names")
@@ -161,8 +147,13 @@ class SendTorrentDialog:
         self.col2 = gtk.TreeViewColumn('Email')
         
         #add a rows
-        self.liststore.append([False,"Bayless","yo@ya.com"])
-        self.liststore.append([False,"Gumpert","hey@ya.com"])
+        with open('/home/m160426/Desktop/Capstone/Capstone-deluge/deluge/ui/gtkui/dist_list.csv') as csvfile:
+        	reader = csv.DictReader(csvfile)
+        	for row in reader:
+        		print(row['toggle_box'], row['last_name'], row['email'])
+        		self.liststore.append([row['toggle_box'],row['last_name'],row['email']])
+        #self.liststore.append([False,"Bayless","yo@ya.com"])
+        #self.liststore.append([False,"Gumpert","hey@ya.com"])
         
         #add columns to treeview
         #self.treeview.append_column(self.col0)
@@ -184,12 +175,33 @@ class SendTorrentDialog:
         self.treeview.set_model(self.liststore)
         
         #allow sorting
-        self.col1.set_sort_column_id(0)
+        #self.col1.set_sort_column_id(0)
         
         self.treeview.get_selection().set_mode(gtk.SELECTION_MULTIPLE)
+        #self.listview_torrents.get_selection().connect("changed", self._on_torrent_changed)
         
-        self.dialog.show()
-    
+        
+        self.core_keys = [
+            "pre_allocate_storage",
+            "max_connections_per_torrent",
+            "max_upload_slots_per_torrent",
+            "max_upload_speed_per_torrent",
+            "max_download_speed_per_torrent",
+            "prioritize_first_last_pieces",
+            "sequential_download",
+            "add_paused",
+            "download_location",
+            "download_location_paths_list",
+            "move_completed",
+            "move_completed_path",
+            "move_completed_paths_list",
+        ]
+       
+        self.dialog.set_transient_for(component.get("MainWindow").window)
+    	self.dialog.present()
+    	
+
+        
     def _on_file_toggled(self, render, path):
         (model, paths) = self.treeview.get_selection().get_selected_rows()
         if len(paths) > 1:
@@ -234,8 +246,19 @@ class SendTorrentDialog:
                     
     def _on_cancel_clicked(self, widget):
         log.debug("_on_button_cancel_clicked")
-        self.dialog.destroy()
+        self.hide()
     
+    def hide(self):
+        self.dialog.hide()
+        self.files = {}
+        self.infos = {}
+        self.options = {}
+        self.previous_selected_torrent = None
+        self.torrent_liststore.clear()
+        self.liststore.clear()
+        self.dialog.set_transient_for(component.get("MainWindow").window)
+        return None
+
     def _on_send_clicked(self,widget):
     	
     	torrent_list = []
@@ -272,7 +295,35 @@ class SendTorrentDialog:
     	
     	cmd = "thunderbird -compose \"to=" + email_str + ",subject='deluge',body='New Torrents to Download',attachment='" + torrent_str + "\""
         print(cmd)
+        self.hide()
         os.system(cmd)
+        
+    def _on_torrent_changed(self, treeselection):
+        (model, row) = treeselection.get_selected()
+        if row is None or not model.iter_is_valid(row):
+            self.liststore.clear()
+            self.previous_selected_torrent = None
+            return
+
+        if model[row][0] not in self.files:
+            self.liststore.clear()
+            self.previous_selected_torrent = None
+            return
+
+        # Save the previous torrents options
+        self.save_torrent_options()
+        # Update files list
+        files_list = self.files[model.get_value(row, 0)]
+
+        self.prepare_file_store(files_list)
+
+        if self.core_config == {}:
+            self.update_core_config()
+
+        # Update the options frame
+        self.update_torrent_options(model.get_value(row, 0))
+
+        self.previous_selected_torrent = row
         
     def _on_torrent_name_edit(self, w, row, new_name):
         # TODO: Update torrent name
@@ -378,6 +429,7 @@ class SendTorrentDialog:
     def save_torrent_options(self, row=None):
         # Keeps the torrent options dictionary up-to-date with what the user has
         # selected.
+        pass
         if row is None:
             if self.previous_selected_torrent and \
                     self.torrent_liststore.iter_is_valid(self.previous_selected_torrent):
@@ -486,14 +538,14 @@ class SendTorrentDialog:
             self.infos[info.info_hash] = info.filedata
             self.listview_torrents.get_selection().select_iter(new_row)
 
-            self.set_default_options()
-            self.save_torrent_options(new_row)
+            #self.set_default_options()
+            #self.save_torrent_options(new_row)
 
         (model, row) = self.listview_torrents.get_selection().get_selected()
         if not row and new_row:
             self.listview_torrents.get_selection().select_iter(new_row)
 
-        self.builder.get_object("label_torrent_count").set_text("Torrents (%d)" % len(self.torrent_liststore))
+        #self.builder.get_object("label_torrent_count").set_text("Torrents (%d)" % len(self.torrent_liststore))
 
         if already_added:
             log.debug("Tried to add %d duplicate torrents!", already_added)
